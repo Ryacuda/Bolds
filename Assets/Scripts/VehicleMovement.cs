cb5066d075cb1bb3775dc0016d6d35d1fe2c9b6b
Adam Germain <55671330+Ryacuda@users.noreply.github.com>
2023-11-22 15:58:17 +0100

    sprite sheet selection

---
+++ Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class VehicleMovement : MonoBehaviour
+{
+	[SerializeField] private float turning_speed;       // deg/sec
+	[SerializeField] private float max_speed;           // unit/sec
+	public float direction;								// deg
+
+	public float speed;                                 // unit/sec
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        Vector3 desired_direction = Input.mousePosition - new Vector3(Screen.width/2, Screen.height/2, 0f);
+		desired_direction.Normalize();
+
+		direction = Mathf.Atan2(desired_direction.y, desired_direction.x);
+
+        transform.position += Time.deltaTime * speed * desired_direction;
+	}
+}
--- Assets/Scripts/VehicleSpriteHandler.cs
+++ Assets/Scripts/VehicleSpriteHandler.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class VehicleSpriteHandler : MonoBehaviour
 {
-    [SerializeField] private Vector3 sprite_offset;
-    private Transform sprite_transform;
+	[SerializeField] private Vector3 sprite_offset;
+	private Transform sprite_transform;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        sprite_transform = gameObject.transform.Find("sprite");
-    }
+	private float sprite_cell_size;
+	private int sheet_size;
+	private int number_of_cells;
+    private int pixel_per_unit;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(transform.forward);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		sprite_transform = gameObject.transform.Find("sprite");
+
+		sprite_cell_size = 210;
+		sheet_size = 7;
+		number_of_cells = 48;
+		pixel_per_unit = 32;
+
+		// test offset
+		//sprite_transform.position = transform.position + sprite_offset;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// get the current vehicle direction
+		float angle = gameObject.GetComponent<VehicleMovement>().direction;
+
+		// index in the sprite sheet
+		int i = Mathf.FloorToInt(angle / number_of_cells);
+
+		// position in the sprite sheet
+		int x = i % sheet_size;
+		int y = Mathf.FloorToInt(i / sheet_size);
+
+		// pivot is center
+		x -= 3;
+		y -= 3;
+
+		Debug.Log(x);
+		Debug.Log(y);
+
+		float mult = sprite_cell_size / pixel_per_unit;
+		sprite_offset = new Vector3(x * mult, y * mult);
+
+		sprite_transform.position = transform.position + sprite_offset;
+	}
 }
